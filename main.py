61b77cf0767a7db72a6591da4293e3f146af55f5
DanielMoreiraFr <moreiradefranca.daniel@gmail.com>
2026-04-16 15:41:47 -0300

    opções de login

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from lib.texto import *
+from time import sleep
 
 while True:
-    escolha = menu('Ruralinfo' ,['Login', 'Cadastrar', 'Entrar como visitante', 'Sair'])
+    escolha = menu('RURALINFO' ,['Login', 'Cadastrar', 'Entrar como visitante', 'Sair'])
     if escolha == 1:
-        cabeçalho('Login')
+        tipoLogin = menu('TIPO DE LOGIN', ['Usuário comum', 'Administrador'])
+        if tipoLogin == 1:
+            print('Entrando como usuário comum')
+        elif tipoLogin == 2:
+            print('Entrando como administrador')
+        else:
+            print(f'{cores('vermelho')}ERRO: Digite um número válido!{cores()}')   
     elif escolha == 2:
         cabeçalho('Cadastrar')
     elif escolha == 3:
-        cabeçalho('Entrar como visitante')
+        cabeçalho('Entrando como visitante')
     elif escolha == 4 or escolha == 0:
         cabeçalho('Saindo...')
         break
     else:
         print(f'{cores('vermelho')}ERRO: Digite um número válido!{cores()}')
+    sleep(1.5)
