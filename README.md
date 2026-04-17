ef435edf0fddfd0417c96a474f9e7f40ba7ae061
DanielMoreiraFr <moreiradefranca.daniel@gmail.com>
2026-04-17 14:05:44 -0300

    introdução do SQLite

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# PusMap
+# Ruralinfo
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from lib.texto import *
+from banco.bancoSQLite import *
+from pacote.texto import *
 from time import sleep
 
 while True:
@@ -13,6 +14,7 @@ while True:
             print(f'{cores('vermelho')}ERRO: Digite um número válido!{cores()}')   
     elif escolha == 2:
         cabeçalho('Cadastrar')
+        inserirUsuario('Luck', 'meufilho@gmail.com', 'teamo', 1)
     elif escolha == 3:
         cabeçalho('Entrando como visitante')
     elif escolha == 4 or escolha == 0:
