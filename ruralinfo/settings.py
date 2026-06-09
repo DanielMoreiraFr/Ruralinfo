b1cc202fa7818f1ca6e5135549595ca21f7ae2ad
DanielMoreiraFr <moreiradefranca.daniel@gmail.com>
2026-06-09 19:29:39 -0300

    refac: mudar de Ruralinfo para ruralinfo nas configs do sistema

--- ruralinfo/settings.py
+++ ruralinfo/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Ruralinfo.urls'
+ROOT_URLCONF = 'ruralinfo.urls'
 
 TEMPLATES = [
     {
@@ -64,7 +64,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Ruralinfo.wsgi.application'
+WSGI_APPLICATION = 'ruralinfo.wsgi.application'
 
 DATABASES = {
     'default': {
